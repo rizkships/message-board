218f78a6da4a84b3545443c7cf7807cab96c6262
rizk <rizkdotai@gmail.com>
2024-03-16 01:45:58 +0200

    add POST route to handle new message submissions

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+// routes/index.js
+
 const express = require('express');
 const router = express.Router();
 
@@ -15,17 +17,33 @@ const messages = [
   }
 ];
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // Render index view and pass messages array
-
     res.render('index', { title: 'Message Board', messages: messages });
 });
 
 /* GET new message form. */
 router.get('/new', function(req, res, next) {
-  res.render('form', { title: 'New Message' });
+    res.render('form', { title: 'New Message' });
+});
+
+/* POST new message. */
+router.post('/new', function(req, res, next) {
+    // Extract data from the form submission
+    const { author, message } = req.body;
+
+    // Create a new message object
+    const newMessage = {
+        text: message,
+        user: author,
+        added: new Date()
+    };
+
+    // Add the new message to the messages array
+    messages.push(newMessage);
+
+    // Redirect to the home page
+    res.redirect('/');
 });
 
 module.exports = router;
