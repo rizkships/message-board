5de2f509ff8b3c2550a90a980ee39ddb59271d36
rizk <rizkdotai@gmail.com>
2024-03-16 00:20:43 +0200

    add new message form route and template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// Sample messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // Render index view and pass messages array
+
+    res.render('index', { title: 'Message Board', messages: messages });
+});
+
+/* GET new message form. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
 });
 
 module.exports = router;
